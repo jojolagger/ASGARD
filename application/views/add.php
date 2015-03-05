12519732a2f848945de0f15257a160dd1205ba2d
Alex Dellow <alex_dellow@hotmail.co.uk>
2015-03-05 11:26:50 -0800

    Changed Add to form

--- application/views/add.php
+++ application/views/add.php
@@ -6,11 +6,14 @@
 		<div style="text-align:center; vertical-align:middle; padding: 10%;">
 			<form action="/listing" method="POST">	<input type="text" style="width=40%" />	<input type="submit" value="Search" />	</form>
 			<h2>{acronym}</h2>
-			<h4 style="margin:0 0 0 0;">Expansion</h4>
-			<input type="text" />
-			<h4 style="margin:5% 0 0 0;">Description</h4>
-			<input type="text" /> <br /> <br />
-			<button type="button" style="width:100px; height:50px;">Confirm Add</button>
+			
+			<form action="/add/submit" method="POST">
+			Expansion  <input type="text" />
+			<br />
+			Description <input type="text" />
+			<br />
+			<input type="submit" value="Add">
+			</form>
 			
 		</div>
 	</body>
